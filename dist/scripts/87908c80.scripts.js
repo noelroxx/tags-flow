f7e9074928fd91f71b33cc437aa583f9b26f2dd7
Noel Roxx <noelroxx@outlook.com>
2013-09-07 11:43:47 +0600

    Build first deploy release

---
+++ dist/scripts/87908c80.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("tagsFlowApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("tagsFlowApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
