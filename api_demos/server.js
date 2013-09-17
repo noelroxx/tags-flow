f62616e110075464bfc5a64abf94c1a4e60a01c1
Noel Roxx <noelroxx@outlook.com>
2013-09-17 19:24:59 +0600

    Some cleanUp

--- api_demos/server.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express(),
-	port = process.env.PORT || 3000;
-
-app.use(express.static(__dirname + '/public'));
-app.listen(port);
-console.log('Listening at', port);
